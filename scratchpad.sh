1fb59b987191e00f962c0f5f5ccfcc9d29337f68
tony <tony@count-zero>
2009-06-27 09:24:43 +0100

    savepoint

--- scratchpad.sh
+++ scratchpad.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
 
+get_subjects() {
+    exiftool -Subject "$1" | awk -F: '{print $2}' | sort | uniq
+}
+set_subjects() {
+    exiftool -Subject="$1" "$2"
+}
+IMAGE="Winter/SG104404.JPG"
+
+set_subjects "winter" $IMAGE
+
+subjects=`get_subjects $IMAGE`
+
+echo "Subjects: $subjects"
+
+set_subjects "$subjects, Favourites" $IMAGE
+get_subjects $IMAGE
+set_subjects "$subjects" $IMAGE
+get_subjects $IMAGE
