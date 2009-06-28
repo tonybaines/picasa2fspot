6a0d761fb05ddc9c53cf2b99f1684637a74f6029
tony <tony@count-zero>
2009-06-28 21:49:46 +0100

    savepoint

--- src/groovy/StarredToFavourites.groovy
+++ src/groovy/StarredToFavourites.groovy
@@ -1,9 +1,9 @@
-WORD=/\w_\.\-/
+import java.io.FileNotFoundExceptionWORD=/\w_\.\-/
 IMAGE=/\[([$WORD]+jpg)\]/
 STARRED=/star\=yes/
 STARRED_IMAGES=/(?is:$IMAGE([\s|$WORD=$WORD])+?$STARRED)/
 
-def markAsFavourites(dir) {
+def markAsFavourites(dir) {	println "Processing images ${dir}"
 	new File("${dir}/Picasa.ini").text.eachMatch(STARRED_IMAGES) {
 		def filename = "${dir}/${it[1]}"
 		def exifSubjects = getExifSubjects(filename)
@@ -23,15 +23,19 @@ def addExifSubject(subject, filename) {
 	def uniqueNonEmptySubjects = subjects.unique().findAll {!it.trim().empty}
 	"exiftool -overwrite_original -P -Subject=${uniqueNonEmptySubjects.join(',')} ${filename}".execute()	
 }
-
+def processDir(base) {    base.eachDirRecurse {		println "Checking for Picasa metadata in ${it.absolutePath}"    	if (new File(it.absolutePath + '/Picasa.ini').exists()) {    		markAsFavourites it    	}    }}
 
 def cli = new CliBuilder(usage: 'Specify the directory to process')
 cli.h(longOpt: 'help', 'Usage')
 cli.d(longOpt: 'dir', args: 1, required: true, 'Directory to process')
+cli.r(longOpt: 'recursive', required: false, 'Find all sub-directories with Picasa metadata and process them')
 
 def options = cli.parse(args)
 if (!options) return
 if (options.h) cli.usage()
-
-markAsFavourites options.d
-
+if (options.r) {	processDir(new File(options.d))}else {
+	try {
+		markAsFavourites options.d
+	}
+	catch (FileNotFoundException e) {		System.err.println("${options.d} doesn't have the Picasa.ini metadata file")	}
+}
