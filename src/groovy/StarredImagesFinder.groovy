1dee9cbe75070383abdb43f33f6f8a24090d1dd0
tony <tony@count-zero>
2009-06-28 20:53:28 +0100

    savepoint

--- src/groovy/StarredImagesFinder.groovy
+++ src/groovy/StarredImagesFinder.groovy
@@ -1,14 +1,15 @@
-def String text = new File('Winter/Picasa.ini').text
 WORD=/\w_\.\-/
 IMAGE=/\[([$WORD]+jpg)\]/
 STARRED=/star\=yes/
 STARRED_IMAGES=/(?is:$IMAGE([\s|$WORD=$WORD])+?$STARRED)/
 
-text.eachMatch(STARRED_IMAGES) {
-	def filename = "Winter/${it[1]}"
-	def exifSubjects = getExifSubjects(filename)
-	println "Marking ${filename} as a Favourite"
-	addExifSubject('Favourites', filename)
+def markAsFavourites(dir) {
+	new File("${dir}/Picasa.ini").text.eachMatch(STARRED_IMAGES) {
+		def filename = "${dir}/${it[1]}"
+		def exifSubjects = getExifSubjects(filename)
+		println "Marking ${filename} as a Favourite"
+		addExifSubject('Favourites', filename)
+	}
 }
 
 def getExifSubjects(filename) {
@@ -20,5 +21,17 @@ def addExifSubject(subject, filename) {
 	List subjects = getExifSubjects(filename)
 	subjects += subject
 	def uniqueNonEmptySubjects = subjects.unique().findAll {!it.trim().empty}
-	"exiftool -overwrite_original -Subject=${uniqueNonEmptySubjects.join(',')} ${filename}".execute()	
+	"exiftool -overwrite_original -P -Subject=${uniqueNonEmptySubjects.join(',')} ${filename}".execute()	
 }
+
+
+def cli = new CliBuilder(usage: 'Specify the directory to process')
+cli.h(longOpt: 'help', 'Usage')
+cli.d(longOpt: 'dir', args: 1, required: true, 'Directory to process')
+
+def options = cli.parse(args)
+if (!options) return
+if (options.h) cli.usage()
+
+markAsFavourites options.d
+
