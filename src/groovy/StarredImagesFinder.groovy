16781410c66fbac00de54597be8e7ba26e23ee2d
tony <tony@count-zero>
2009-06-28 16:16:41 +0100

    savepoint

--- src/groovy/StarredImagesFinder.groovy
+++ src/groovy/StarredImagesFinder.groovy
@@ -7,21 +7,18 @@ STARRED_IMAGES=/(?is:$IMAGE([\s|$WORD=$WORD])+?$STARRED)/
 text.eachMatch(STARRED_IMAGES) {
 	def filename = "Winter/${it[1]}"
 	def exifSubjects = getExifSubjects(filename)
-	println "Subjects of ${filename}: ${exifSubjects}"
+	println "Marking ${filename} as a Favourite"
 	addExifSubject('Favourites', filename)
-	println "Subjects of ${filename}: ${getExifSubjects(filename)}"
 }
 
 def getExifSubjects(filename) {
-	def command = "exiftool -Subject ${filename}"
-	command.execute().text.split(/:/)[1].trim().split(/\s*,\s*/)
+	"exiftool -Subject ${filename}".execute().text
+		.split(/:/)[1].trim().split(/\s*,\s*/)
 }
 
 def addExifSubject(subject, filename) {
 	List subjects = getExifSubjects(filename)
 	subjects += subject
 	def uniqueNonEmptySubjects = subjects.unique().findAll {!it.trim().empty}
-	def command = "exiftool -Subject=${uniqueNonEmptySubjects.join(',')} ${filename}"
-	println command
-	command.execute()	
+	"exiftool -Subject=${uniqueNonEmptySubjects.join(',')} ${filename}".execute()	
 }
